270abb4bfe12b3bd089be700797ac76ae804035a
omsant02 <omsantoshwar02@gmail.com>
2023-12-09 13:27:24 +0530

    4- add cleanup function

--- 4-Use Effect/vite-project/src/App.jsx
+++ 4-Use Effect/vite-project/src/App.jsx
@@ -1,26 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("UseEffect called");
-  }, [count1, count2]);
+    console.log("Effect ran");
+    return () => {
+      console.log("Cleaning mess of useEffect");
+      setCount(0);
+    };
+  }, [count]); // Empty dependency array means this effect runs only on mount and unmount
 
   return (
-    <>
-      <h2>{count1}</h2>
-      <button onClick={() => setCount1(count1 + 1)}>Increment Count 1</button>
-      <h2>{count2}</h2>
-      <button onClick={() => setCount2(count2 + 1)}>Increment Count 2</button>
-    </>
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
   );
 }
 
 export default App;
-
-// useEffect(() => {});                 -called after 1st render,  then every render
-// useEffect(() => {}, []);             -called after 1st render,  never called again
-// useEffect(() => {}, [dependencies]); -called after 1st render,  called if dependencies changes
