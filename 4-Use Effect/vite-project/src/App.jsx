033d623bd88839f03b87dd02aa0674f933a0f030
omsant02 <omsantoshwar02@gmail.com>
2023-12-09 13:11:53 +0530

    4- use useEffect

--- 4-Use Effect/vite-project/src/App.jsx
+++ 4-Use Effect/vite-project/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [user, setUser] = useState("");
 
-  const fetchData = async () => {
-    const res = await fetch("https://randomuser.me/api/");
-    const data = await res.json();
-    const jsonString = JSON.stringify(data);
-    setUser(jsonString);
-  };
-  fetchData();
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://randomuser.me/api/");
+      const data = await res.json();
+      const jsonString = JSON.stringify(data);
+      setUser(jsonString);
+    };
+    fetchData();
+  }, []);
 
   return <>{user}</>;
 }
 
 export default App;
+
+// useEffect(() => {});                 -called after 1st render,  then every render
+// useEffect(() => {}, []);             -called after 1st render,  never called again
+// useEffect(() => {}, [dependencies]); -called after 1st render,  called if dependencies changes
