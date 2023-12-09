1ecb1b04f6786418b490559b71514d7dd2430920
omsant02 <omsantoshwar02@gmail.com>
2023-12-09 14:06:45 +0530

    5-Forms (handling form by OnChange Event-can render continuously)

--- 5-Forms/vite-project/src/Login.jsx
+++ 5-Forms/vite-project/src/Login.jsx
@@ -1,30 +1,46 @@
-import "./Login.css";
 import { useState } from "react";
+import "./Login.css";
 const Login = () => {
-  const [user, setUser] = useState("");
-  const [pass, setPass] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleForm = (e) => {
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const userName = document.querySelector("#user").value;
-    console.log(userName);
-    const passWord = document.querySelector("#pass").value;
-    console.log(passWord);
-    setUser(userName);
-    setPass(passWord);
+    console.log("Submitted - Username:", username, "Password:", password);
+    setPassword("");
+    setUsername("");
   };
 
   return (
     <div className="login-container">
       <h2>Login</h2>
-      <form onSubmit={handleForm}>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" required id="user" />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" required id="pass" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
         </div>
         <button type="submit">Login</button>
       </form>
