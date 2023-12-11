3f10e2933f3daa30852b40c63d312ce633a4d193
omsant02 <omsantoshwar02@gmail.com>
2023-12-11 14:14:24 +0530

    7- add usestate

--- 7-useContext/vite-project/src/components/UserCard.jsx
+++ 7-useContext/vite-project/src/components/UserCard.jsx
@@ -1,15 +1,16 @@
-import "./UserCard.css";
-import userContext from "../context/userContext";
 import { useContext } from "react";
+import userContext from "../context/userContext";
+import "./UserCard.css";
 
 const UserCard = () => {
-  const user = useContext(userContext);
+  const pointer = useContext(userContext);
   return (
     <div className="user-card">
       <h3>User Card</h3>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
-      <p>Age: {user.age}</p>
+      <p>Name: {pointer.user.name}</p>
+      <p>Email: {pointer.user.email}</p>
+      <p>Age: {pointer.user.age}</p>
+      <button onClick={pointer.changeUser}>Change User</button>
     </div>
   );
 };
--- 7-useContext/vite-project/src/context/UserInfo.jsx
+++ 7-useContext/vite-project/src/context/UserInfo.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { useState } from "react";
 import UserContext from "./userContext";
-
 const UserInfo = ({ children }) => {
-  const user = {
+  const [user, setUser] = useState({
     name: "John Doe",
     email: "john.doe@example.com",
     age: 30,
+  });
+
+  const changeUser = () => {
+    setUser({
+      name: "XYZ",
+      email: "xyz@example.com",
+      age: 20,
+    });
   };
 
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider value={{ user, changeUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
-
 export default UserInfo;
