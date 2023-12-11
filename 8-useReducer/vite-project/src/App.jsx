b7e260c79829c77c45e77be6d7cfafb1a58476cf
omsant02 <omsantoshwar02@gmail.com>
2023-12-11 14:45:18 +0530

    8- add actions

--- 8-useReducer/vite-project/src/App.jsx
+++ 8-useReducer/vite-project/src/App.jsx
@@ -3,19 +3,35 @@ import { useReducer } from "react";
 import "./App.css";
 
 const reducer = (state, action) => {
-  return { count: state.count + 1 };
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
+    case "RESET":
+      return { count: 0 };
+    default:
+      throw new Error("Invalid action type");
+  }
 };
-
 function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   const increment = () => {
-    dispatch();
+    dispatch({ type: "INCREMENT" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  const reset = () => {
+    dispatch({ type: "RESET" });
   };
   return (
     <div>
       <p>Count : {state.count}</p>
       <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
